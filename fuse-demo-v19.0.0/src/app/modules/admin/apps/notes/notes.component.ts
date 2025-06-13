9d1c561aacec0b2b93b4c4c6014e679d10100ba8
Karimov Islom <71114810+Islom1999@users.noreply.github.com>
2025-06-13 10:02:53 +0500

    Angular Fuse v19

---
+++ fuse-demo-v19.0.0/src/app/modules/admin/apps/notes/notes.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'notes',
+    templateUrl: './notes.component.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [RouterOutlet]
+})
+export class NotesComponent
+{
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+}
---
+++ fuse-demo-v19.0.0/src/app/modules/admin/ui/page-layouts/overview/overview.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+    selector: 'overview',
+    templateUrl: './overview.component.html',
+    encapsulation: ViewEncapsulation.None,
+    imports: [MatIconModule]
+})
+export class OverviewComponent
+{
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+    }
+}
