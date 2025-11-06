3c2018d7b3ee4e4592ee9857e1d845ad46de617a
Karimov Islom <71114810+Islom1999@users.noreply.github.com>
2025-11-06 10:13:35 +0500

    chore: update package.json to add linting and formatting tools; refactor home component for consistency

--- fuse-starter-v19.0.0/src/app/modules/landing/home/home.component.ts
+++ fuse-starter-v19.0.0/src/app/modules/landing/home/home.component.ts
@@ -1,20 +1,17 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { FuseIconComponent } from '@fuse/components/icon';
+import { Component, ViewEncapsulation } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
+import { FuseIconComponent } from '@fuse/components/icon'
 
 @Component({
-    selector: 'landing-home',
-    templateUrl: './home.component.html',
-    encapsulation: ViewEncapsulation.None,
-    imports: [ButtonModule, RouterLink, FuseIconComponent]
+  selector: 'app-landing-home',
+  templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.None,
+  imports: [ButtonModule, RouterLink, FuseIconComponent],
 })
-export class LandingHomeComponent
-{
-    /**
-     * Constructor
-     */
-    constructor()
-    {
-    }
+export class LandingHomeComponent {
+  /**
+   * Constructor
+   */
+  constructor() {}
 }
